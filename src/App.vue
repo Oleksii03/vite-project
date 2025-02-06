03f72a942ae2b8c7d887baba220b714cd89d94b1
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2025-02-06 16:57:59 +0200

    add-todo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,74 @@
-<script setup></script>
+<script>
+// let id = 0;
 
-<template></template>
+// export default {
+//   data() {
+//     return {
+//       newTodo: "",
+//       todos: [
+//         { id: id++, text: "Вивчити HTML" },
+//         { id: id++, text: "Вивчити JavaScript" },
+//         { id: id++, text: "Вивчити Vue" },
+//       ],
+//     };
+//   },
+//   methods: {
+//     addTodo() {
+//       if (!this.newTodo.trim()) return;
 
-<style scoped lang="scss">
-@import "./assets/styles/main.scss";
-</style>
+//       this.todos.push({
+//         id: id++,
+//         text: this.newTodo,
+//       });
+
+//       this.newTodo = "";
+//     },
+//     removeTodo(todo) {
+//       this.todos = this.todos.filter((item) => item.id !== todo.id);
+//     },
+//   },
+// };
+</script>
+
+// --------Composition API-------------
+
+<script setup>
+import { ref } from "vue";
+
+let id = 0;
+
+const newTodo = ref("");
+const todos = ref([
+  { id: id++, text: "Вивчити HTML" },
+  { id: id++, text: "Вивчити JavaScript" },
+  { id: id++, text: "Вивчити Vue" },
+]);
+
+function addTodo() {
+  if (!newTodo.value) return;
+
+  todos.value.push({
+    id: id++,
+    text: newTodo.value,
+  });
+
+  newTodo.value = "";
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((item) => item.id !== todo.id);
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
+    <button>Додати завдання</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+</template>
